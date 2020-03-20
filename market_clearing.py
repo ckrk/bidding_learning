0ecc95f1c6ca6933d4626ddd549a2f92a9d84bc9
viktorzob <viktor.zobernig@gmail.com>
2020-03-20 09:30:14 +0100

    Update market_clearing.py

--- market_clearing.py
+++ market_clearing.py
@@ -7,18 +7,18 @@ import numpy as np
 
 a = np.array([0,5,10])
 b = np.array([1,2,10])
-<<<<<<< HEAD
+
 c = np.array([2,3,10])
 d = np.array([1,9,10])
 test_array = np.stack((a,b,d,a,b,c,b,c))
 #test_array = np.stack((a,b,c))
-=======
+
 c = np.array([2,3,11])
 d = np.array([1,9,7])
 #test_array = np.stack((a,b,d,a,b,c,b,c))
 test_array = np.stack((a,b,d,c))
 
->>>>>>> 256a1d54319b37a6f5a90fd23116bde8f8337379
+
 
 print(test_array)
 def market_clearing(self,demand,bids):    
@@ -71,9 +71,9 @@ def market_clearing(self,demand,bids):
     #print(market_price)
     return market_price,bids
 
-<<<<<<< HEAD
+
 market_clearing(_,37,test_array)
-=======
+
 market_clearing(_,17,test_array)
 
 ''' 
@@ -83,6 +83,6 @@ Possible Testcases:
     Check 3 cases low demand, equal demanl and high demand
 '''
 
->>>>>>> 256a1d54319b37a6f5a90fd23116bde8f8337379
+
 
     

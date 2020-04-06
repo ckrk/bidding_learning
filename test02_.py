e62ff49d740dd1611eda2eaf2649e8bc4fb70a60
viktorzob <viktor.zobernig@gmail.com>
2020-04-06 12:55:26 +0200

    required for 2 Player Experiments now

--- test02_.py
+++ test02_.py
@@ -3,6 +3,10 @@
 Created on Wed Mar  4 10:05:17 2020
 
 @author: Viktor
+
+for 2-Player Games
+works for EnMarketEnv_2Player01 and DDPG02_
+
 """
 
 ###### Versuch mit 2 agents ########################
@@ -14,22 +18,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 from DDPG02_ import DDPGagent02
 from utils_ import OUNoise, Memory
-from EnMarketEnv06_ import EnMarketEnv06 
+from EnMarketEnv_2Player01 import EnMarketEnv_2Player01
 
 
 
 
-env = EnMarketEnv06(CAP = np.array([400,500,600]), costs = np.array([100,100,100]))
+env = EnMarketEnv_2Player01(CAP = np.array([50,50]), costs = np.array([20,20]), Rewards=3)
 
 
 
 agent0 = DDPGagent02(env)
 agent1 = DDPGagent02(env)
-agent2 = DDPGagent02(env)
+#agent2 = DDPGagent02(env)
 noise = OUNoise(env.action_space)
 batch_size = 128
 rewards = []
 avg_rewards = []
+last_action = np.array([0,0,0])
+#last_rewards = np.array([0,0,0])
+#soldCAP = np.array([0,0,0])
 
 for episode in range(50):
     state = env.reset()
@@ -42,22 +49,22 @@ for episode in range(50):
         action0 = noise.get_action(action0, step)
         action1 = agent1.get_action(state)
         action1 = noise.get_action(action1, step)
-        action2 = agent2.get_action(state)
-        action2 = noise.get_action(action2, step)
+        #action2 = agent2.get_action(state)
+        #action2 = noise.get_action(action2, step)
         
-        action = np.concatenate([action0, action1, action2])
-        new_state, reward, done, _ = env.step(action)   
+        action = np.concatenate([action0, action1])#, action2])
+        new_state, reward, done, _ = env.step(action, last_action)   
         
         agent0.memory.push(state, np.array([action[0]]), np.array([reward[0]]), new_state, done)
         agent1.memory.push(state, np.array([action[1]]), np.array([reward[1]]), new_state, done)
-        agent2.memory.push(state, np.array([action[2]]), np.array([reward[2]]), new_state, done)
+        #agent2.memory.push(state, np.array([action[2]]), np.array([reward[2]]), new_state, done)
 
 
         
         if len(agent0.memory) > batch_size:            
             agent0.update(batch_size)
             agent1.update(batch_size)
-            agent2.update(batch_size) 
+            #agent2.update(batch_size) 
         
         state = new_state
         episode_reward += reward

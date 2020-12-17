d1cc2b23dcb5711a657acbccfb3ecf1f0479f6b3
viktorzob <viktor.zobernig@gmail.com>
2020-12-17 08:58:45 +0100

    cuda- availability fixed to work also on cpu

--- src/agent_ddpg_cuda.py
+++ src/agent_ddpg_cuda.py
@@ -4,7 +4,7 @@ import torch.optim as optim
 import torch.nn as nn
 from torch.autograd import Variable
 
-from src.actor_criticTEST import Actor, Critic
+from src.actor_critic import Actor, Critic
 from src.utils import Memory
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -51,15 +51,16 @@ class agent_ddpg:
     
     def update(self, batch_size):
         states, actions, rewards, next_states, _ = self.memory.sample(batch_size)
-        states = torch.cuda.FloatTensor(states, device=device)
-        actions = torch.cuda.FloatTensor(actions, device=device)
-        rewards = torch.cuda.FloatTensor(rewards, device=device)
-        next_states = torch.cuda.FloatTensor(next_states, device=device)
         
-        #states = torch.FloatTensor(states, device=device)
-        #actions = torch.FloatTensor(actions, device=device)
-        #rewards = torch.FloatTensor(rewards, device=device)
-        #next_states = torch.FloatTensor(next_states, device=device)
+        #states = torch.cuda.FloatTensor(states, device=device)
+        #actions = torch.cuda.FloatTensor(actions, device=device)
+        #rewards = torch.cuda.FloatTensor(rewards, device=device)
+        #next_states = torch.cuda.FloatTensor(next_states, device=device)
+        
+        states = torch.FloatTensor(states, device=device)
+        actions = torch.FloatTensor(actions, device=device)
+        rewards = torch.FloatTensor(rewards, device=device)
+        next_states = torch.FloatTensor(next_states, device=device)
         
         #states = torch.from_numpy(states).float().to(device)
         #actions = torch.from_numpy(actions).float().to(device)
@@ -72,19 +73,21 @@ class agent_ddpg:
         next_actions = self.actor_target.forward(next_states)
         next_Q = self.critic_target.forward(next_states, next_actions.detach())
         Qprime = rewards + self.gamma * next_Q
-        critic_loss = self.critic_criterion(Qvals, Qprime) 
+        #critic_loss = self.critic_criterion(Qvals, Qprime) 
 
         # Actor loss
-        policy_loss = -self.critic.forward(states, self.actor.forward(states)).mean()
+        #policy_loss = -self.critic.forward(states, self.actor.forward(states)).mean()
         
         # update networks
-        self.actor_optimizer.zero_grad()
-        policy_loss.backward()
-        self.actor_optimizer.step()
-
         self.critic_optimizer.zero_grad()
+        critic_loss = self.critic_criterion(Qvals, Qprime)
         critic_loss.backward() 
         self.critic_optimizer.step()
+        
+        self.actor_optimizer.zero_grad()
+        policy_loss = -self.critic.forward(states, self.actor.forward(states)).mean()
+        policy_loss.backward()
+        self.actor_optimizer.step()
 
         # update target networks 
         for target_param, param in zip(self.actor_target.parameters(), self.actor.parameters()):

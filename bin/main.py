d769544f911008f268e95a14c305cadee5e7e970
c <uni@chrisgraf.net>
2021-02-01 11:20:16 -0800

    clean-up

--- bin/main.py
+++ bin/main.py
@@ -1,8 +1,8 @@
-import os
-path = os.path.dirname(os.path.realpath("__file__"))
+import os, sys
+path = os.path.dirname(os.path.realpath(__file__))
 os.chdir(path)
+sys.path.append(os.path.dirname(path))
 
-import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from PyPDF2 import PdfFileMerger
@@ -11,10 +11,8 @@ import pickle
 import datetime
 import time
 
-from src.utils import  UniformNoise, OUNoise, GaussianNoise, plot_run_outcome
+from src.utils import UniformNoise, OUNoise, GaussianNoise, plot_run_outcome
 from src.environment_bid_market import EnvironmentBidMarket
-
-
 
 '''
 High-Level Interface that calls learning algorithm and Energy-Market Environment 
@@ -53,36 +51,36 @@ Discrete:    binary
 Enables Discrete Spaces (Not yet functional)
 '''
 # if plots should be saved
-pdfs =[]
+pdfs = []
 # save date/time
 time_stamp = datetime.datetime.now()
 meta_data_time = time_stamp.strftime('%d-%m-%y %H:%M')
 
 # Agent Parameters
-POWER_CAPACITIES = [50/100,50/100] #50
-PRODUCTION_COSTS = [20/100,20/100] #20
-DEMAND = [70/100,70/100] #70
-ACTION_LIMITS = [-1,1] #[-10/100,100/100]#[-100/100,100/100]
+POWER_CAPACITIES = [50 / 100, 50 / 100]  # 50
+PRODUCTION_COSTS = [20 / 100, 20 / 100]  # 20
+DEMAND = [70 / 100, 70 / 100]  # 70
+ACTION_LIMITS = [-1, 1]  # [-10/100,100/100]#[-100/100,100/100]
 NUMBER_OF_AGENTS = 2
 PAST_ACTION = 0
 FRINGE = 1
 
 # Neural Network Parameters
 # rescaling the rewards to avoid hard weight Updates of the Criticer 
-REWARD_SCALING = 1 #0.01 #
+REWARD_SCALING = 1  # 0.01 #
 LEARNING_RATE_ACTOR = 1e-4
 LEARNING_RATE_CRITIC = 1e-3
-NORMALIZATION_METHOD = 'none' # options are BN = Batch Normalization, LN = Layer Normalization, none
+NORMALIZATION_METHOD = 'none'  # options are BN = Batch Normalization, LN = Layer Normalization, none
 
 # Noise Parameters
-NOISE ='GaussianNoise'  #Options are: 'GaussianNoise',OUNoise','UniformNoise'
-DECAY_RATE = 0.001 #0.0004 strong; 0.0008 medium; 0.001 soft; # if 0: Not used, if:1: only simple Noise without decay used
-REGULATION_COEFFICENT = 10 # if 1: Not used, if:0: only simple Noise used
+NOISE = 'GaussianNoise'  # Options are: 'GaussianNoise',OUNoise','UniformNoise'
+DECAY_RATE = 0.001  # 0.0004 strong; 0.0008 medium; 0.001 soft; # if 0: Not used, if:1: only simple Noise without decay used
+REGULATION_COEFFICENT = 10  # if 1: Not used, if:0: only simple Noise used
 
-TOTAL_TEST_RUNS = 1 # How many runs should be executed
-EPISODES_PER_TEST_RUN = 5000 # How many episodes should one run contain
-ROUNDS_PER_EPISODE = 1 # How many rounds are allowed per episode (right now number of rounds has no impact -due 'done' is executed if step >= round- and choosing 1 is easier to interpret; )
-BATCH_SIZE = 128 # *0.5 # *2
+TOTAL_TEST_RUNS = 1  # How many runs should be executed
+EPISODES_PER_TEST_RUN = 5000  # How many episodes should one run contain
+ROUNDS_PER_EPISODE = 1  # How many rounds are allowed per episode (right now number of rounds has no impact -due 'done' is executed if step >= round- and choosing 1 is easier to interpret; )
+BATCH_SIZE = 128  # *0.5 # *2
 
 # Dictionary to save data and Parameter settings
 Results = {}
@@ -93,7 +91,7 @@ Results['meta-data'] = {
         'demand':DEMAND,
         'noise': NOISE,
         'regulation_coef':REGULATION_COEFFICENT,
-        'decay_rate' :DECAY_RATE,
+        'decay_rate':DECAY_RATE,
         'lr_critic':LEARNING_RATE_CRITIC,
         'lr_actor':LEARNING_RATE_ACTOR,
         'Normalization': NORMALIZATION_METHOD,
@@ -107,44 +105,42 @@ Results['meta-data'] = {
         'past_action': PAST_ACTION,
         'fringe:player': FRINGE}
 
-
 for test_run in  range(TOTAL_TEST_RUNS):
     print('Test Run: {}'.format(test_run))
     
     # save runtime
-    Results[test_run] ={'runtime':0}
+    Results[test_run] = {'runtime':0}
     t_0 = time.time()
     
     # set up environment
-    env = EnvironmentBidMarket(capacities = POWER_CAPACITIES, costs = PRODUCTION_COSTS, demand = DEMAND, agents = NUMBER_OF_AGENTS, 
-                               fringe_player = FRINGE, past_action= PAST_ACTION, lr_actor = LEARNING_RATE_ACTOR, lr_critic = LEARNING_RATE_CRITIC, 
-                               normalization = NORMALIZATION_METHOD, reward_scaling = REWARD_SCALING, action_limits = ACTION_LIMITS, rounds_per_episode = ROUNDS_PER_EPISODE)
+    env = EnvironmentBidMarket(capacities=POWER_CAPACITIES, costs=PRODUCTION_COSTS, demand=DEMAND, agents=NUMBER_OF_AGENTS,
+                               fringe_player=FRINGE, past_action=PAST_ACTION, lr_actor=LEARNING_RATE_ACTOR, lr_critic=LEARNING_RATE_CRITIC,
+                               normalization=NORMALIZATION_METHOD, reward_scaling=REWARD_SCALING, action_limits=ACTION_LIMITS, rounds_per_episode=ROUNDS_PER_EPISODE)
     # set up agents
     agents = env.create_agents(env)
     
     # set up noise
     if NOISE == 'GaussianNoise':
-        noise = GaussianNoise(env.action_space, mu= 0, sigma = 0.1, regulation_coef= REGULATION_COEFFICENT, decay_rate = DECAY_RATE) # Gaussian Noise (only instead of Ornstein Uhlenbeck Noise)
+        noise = GaussianNoise(env.action_space, mu=0, sigma=0.1, regulation_coef=REGULATION_COEFFICENT, decay_rate=DECAY_RATE)  # Gaussian Noise (only instead of Ornstein Uhlenbeck Noise)
     elif NOISE == 'OUNoise':
-        noise = OUNoise(env.action_space, mu=0.0, theta=0.15, max_sigma=0.3, min_sigma=0.3, decay_period=100000) # Ornstein Uhlenbeck Noise ( only instead of Gaussian Noise)
+        noise = OUNoise(env.action_space, mu=0.0, theta=0.15, max_sigma=0.3, min_sigma=0.3, decay_period=100000)  # Ornstein Uhlenbeck Noise ( only instead of Gaussian Noise)
     elif NOISE == 'UniformNoise':
-        noise = UniformNoise(env.action_space, initial_exploration = 0.99, final_exploration = 0.05, decay_rate = 0.999)
-
+        noise = UniformNoise(env.action_space, initial_exploration=0.99, final_exploration=0.05, decay_rate=0.999)
     
     for episode in range(EPISODES_PER_TEST_RUN):
         Results[test_run][episode] = {'rewards':[], 'actions':[], 'market_price':[] , 'sold_quantities':[],
                                       'round':[], 'state':[], 'new_state':[]}
         # reset noise and state (past_actions resets only at the bginning of a new run)
         state = env.reset(episode)
-        noise.reset() # only important for OUNoise 
-
+        noise.reset()  # only important for OUNoise
+        
         for step in range(ROUNDS_PER_EPISODE):
             actions = []
             for n in range(len(agents)):
                 action_temp = agents[n].get_action(state)
                 action_temp = noise.get_action(action_temp, episode)
                 actions.append(action_temp[:])
-        
+            
             actions = np.asarray(actions)
             
             # get reward an new state from environment
@@ -153,7 +149,7 @@ for test_run in  range(TOTAL_TEST_RUNS):
             # save data in memory
             for n in range(len(agents)):
                 agents[n].memory.push(state, actions[n], np.array([reward[n]]), new_state, done)
-       
+            
             # update
             if len(agents[0].memory) > BATCH_SIZE:
                 for n in range(len(agents)):
@@ -171,16 +167,14 @@ for test_run in  range(TOTAL_TEST_RUNS):
             # new_state becomes state
             state = new_state
             
-            
             if done:
                 # use some rendering to get insights during running (turned off to save time). 
                 # can be adjusted directly in the environment
-                #sys.stdout.write("***TestRound: {}, episode: {}, reward: {}, average _reward: {} \n".format(test_run, episode, np.round(episode_reward, decimals=2), np.mean(rewards[-10:])))
-                #env.render()
+                # sys.stdout.write("***TestRound: {}, episode: {}, reward: {}, average _reward: {} \n".format(test_run, episode, np.round(episode_reward, decimals=2), np.mean(rewards[-10:])))
+                # env.render()
                 break
-
         
-
+    
     # save running tim in dictionary
     t_end = time.time()
     time_total = t_end - t_0
@@ -191,20 +185,18 @@ for test_run in  range(TOTAL_TEST_RUNS):
     # the total number of episodes per run, the actual run, which curves shoul be plotted (options: 'actions', rewards' or 'both'),
     # a title for the plot, rescale parameters if needed (usage: rescale[param for actions, param for rewards, param for bid limit])
     # and a window "moving_window" for which a moving median gets computed (recommended for presntation reasons) 
-    plot_run_outcome(Results, NUMBER_OF_AGENTS, ACTION_LIMITS[1], 52, 
-                       EPISODES_PER_TEST_RUN, test_run, curves = 'actions', 
-                       title = 'Norm:{} Agents:{} PA: {} Fringe: {}, Run:{}'.format(NORMALIZATION_METHOD,NUMBER_OF_AGENTS, PAST_ACTION, FRINGE, test_run),
-                       rescale=[100,1000,100], moving_window=9)
-    
+    plot_run_outcome(Results, NUMBER_OF_AGENTS, ACTION_LIMITS[1], 52,
+                       EPISODES_PER_TEST_RUN, test_run, curves='actions',
+                       title='Norm:{} Agents:{} PA: {} Fringe: {}, Run:{}'.format(NORMALIZATION_METHOD, NUMBER_OF_AGENTS, PAST_ACTION, FRINGE, test_run),
+                       rescale=[100, 1000, 100], moving_window=9)
     
     # save plots (uncommment ALL below)
-    #plt.savefig('temp{}.pdf'.format(test_run))
-    #plt.close()
-    #pdfs.append('temp{}.pdf'.format(test_run))
-
+    # plt.savefig('temp{}.pdf'.format(test_run))
+    # plt.close()
+    # pdfs.append('temp{}.pdf'.format(test_run))
 
 '''
-### Merg PDFs and pickle
+### Merge PDFs and pickle
 with open('{}_results_lra{}_lrc{}_Agents{}_PA{}_00.pkl'.format(NORMALIZATION_METHOD,NUMBER_OF_AGENTS,LEARNING_RATE_ACTOR,LEARNING_RATE_CRITIC, PAST_ACTION), 'wb') as pickle_file:
     pickle.dump(Results, pickle_file)
 

e16e1920e3e357b0c1e866fd896ff8ce6e308f47
viktorzob <viktor.zobernig@gmail.com>
2022-01-14 11:21:11 +0100

    added to alternative main setups (2/3)

--- bin/rangl_main_2.py
+++ bin/rangl_main_2.py
@@ -8,8 +8,9 @@ sys.path.append(os.path.join(path_parent_folder,'netzerotc'))
 
 import operator 
 from comet_ml import Experiment
+#import logging
 
-#import pandas as pd
+# pandas as pd
 import numpy as np
 import gym
 
@@ -17,7 +18,9 @@ import gym
 import rangl 
 
 from src.agent_ddpg import agent_ddpg
-from src.noise_models import UniformNoise, OUNoise, GaussianNoise 
+from src.noise_models import UniformNoise, OUNoise, GaussianNoise
+import matplotlib.pyplot as plt
+
 
 #from pathlib import Path
 
@@ -25,20 +28,24 @@ from src.noise_models import UniformNoise, OUNoise, GaussianNoise
 env = gym.make("rangl:nztc-open-loop-v0")
 
 
-# Hyper parameters for DDPG
-BATCH_SIZE = 120
-ACTOR_LR = 1e-5
+# Hyper Parameters for DDPG
+BATCH_SIZE = 128
+ACTOR_LR =1e-4
 CRITIC_LR = 1e-3
 GAMMA = 0.99
 
-# Hyper paramters Noise
+
+# Hyper parameters for the noise 
 REGULATION_COEFFICENT = 1 # only moves the variance (if =1: sigma stays the same)
 DECAY_RATE = 1 # basically no decay rate gets apllied
-NOISE_VARIANCE=4
+NOISE_VARIANCE = 4
 
-# Random Guassian Noise gets added to the actions for exploratation 
+# Random Guassian Noise gets added to the actions for exploratation
 noise = GaussianNoise(env.action_space, mu=0, sigma=NOISE_VARIANCE, regulation_coef=REGULATION_COEFFICENT, decay_rate=DECAY_RATE)
-
+# Noise adjustement for "1 Round Game"
+noise.action_dim = noise.action_dim * 20
+noise.low = np.asarray(noise.low.tolist()*20)
+noise.high = np.asarray(noise.high.tolist()*20)
 
 # Reset the environment
 env.reset()
@@ -46,8 +53,10 @@ done = False
 
 # Create agent-ddpg
 "increase max memory size ?"
-
 agent = agent_ddpg(env, hidden_size=[400, 300], actor_learning_rate=ACTOR_LR, critic_learning_rate=CRITIC_LR, gamma=GAMMA, tau=1e-3, max_memory_size=50000, norm = 'none')
+
+#Create individual state
+ind_state = (1,)
 
 # comet logging
 experiment = Experiment(project_name="rangl-challenge-2022",
@@ -58,53 +67,83 @@ experiment.log_parameter('Learning Rate (Actor)', ACTOR_LR)
 experiment.log_parameter('Learning Rate (Critic)',CRITIC_LR)
 experiment.log_parameter('Gamma', GAMMA)
 
+# saving outcome as list
+all_rewards=[]
+
 # Training
 for step in range(1000):
+    
+    cum_reward = 0
+    a1 = 0
+    a3 = 3
+    #env.seed(111)
     env.reset()
     done = False 
     
     print("step:",step)
     
+    action = agent.get_action(np.asarray([ind_state[0]]))
+    action = noise.get_action(action)
+    #print(action)
+    
     while not done:
         
-        action = agent.get_action(np.asarray([env.state.to_observation()[0]]))
-        #action = agent.get_action(env.state.to_observation())
-        action = noise.get_action(action)
-        print(action)
+        action_per_round = action[a1:a3]
         
         # Specify the action. Check the effect of any fixed policy by specifying the action here:
-        observation, reward, done, _ = env.step(action)
+        observation, reward, done, _ = env.step(action_per_round)
+        #print(reward)
         
-        #Saves the played round as tuple in the memory
-        agent.memory.push(tuple(map(operator.sub, observation, (1,))), action, reward, observation, done)
-        
-        if len(agent.memory) > BATCH_SIZE:
-            agent.update(BATCH_SIZE)
+        cum_reward += reward
+        all_rewards.append(reward)
+        a1 += 3
+        a3 += 3
+
+     #Saves the played round as tuple in the memory
+    agent.memory.push(ind_state, action, cum_reward/20, ind_state, done)
     
-    #if len(agent.memory) > BATCH_SIZE:
-        #agent.update(BATCH_SIZE)
-        
+    if len(agent.memory) > BATCH_SIZE:
+        agent.update(BATCH_SIZE)
+
+
+
+
+# plotting reward progress
+#moving median of n_th step (takes median from n rows and outputs an array of the same length as the input)
+med_rewards=[]
+n=19
+for i in range(1000*20):
+    temp_rewards =np.mean(all_rewards[i:n], axis=0)
+    med_rewards.append(temp_rewards)
+    n += 1
+
+recompiled_rewards = np.asarray(med_rewards)
+plt.plot(all_rewards)
+plt.plot(recompiled_rewards)
         
 # Testing/Evaluation
 # Reset the environment
+#env.seed(111)
 env.reset()
 done = False
 
+a1 = 0
+a3 = 3
+action = agent.get_action(np.asarray([env.state.to_observation()[0]]))
+
 while not done:
     
-    action = agent.get_action(np.asarray([env.state.to_observation()[0]]))
-    
+    action_per_round = action[a1:a3]
+    print(action_per_round)
     # Specify the action. Check the effect of any fixed policy by specifying the action here:
-    observation, reward, done, _ = env.step(action)
+    observation, reward, done, _ = env.step(action_per_round)
     
-    agent.memory.push(tuple(map(operator.sub, observation, (1,))), action, reward, observation, done)
+    a1 += 3
+    a3 += 3
     
-   #if len(agent.memory) > BATCH_SIZE: # necessary??
-        #agent.update(BATCH_SIZE)
-
 
 # Plot the episode
 # Ploting works only if the environment wasn`t reseted before plottting !!! (So better plot only after a Test/Evaluation run)
-env.plot("fixed_policy_DirectDeployment.png")
+env.plot("fixed_policy_DirectDeployment_avgReward_1Action_Noise4_gamma99_.png")
 
 experiment.end()
--- bin/rangl_main_3.py
+++ bin/rangl_main_3.py
@@ -8,6 +8,7 @@ sys.path.append(os.path.join(path_parent_folder,'netzerotc'))
 
 import operator 
 from comet_ml import Experiment
+#import logging
 
 #import pandas as pd
 import numpy as np
@@ -26,19 +27,18 @@ env = gym.make("rangl:nztc-open-loop-v0")
 
 
 # Hyper parameters for DDPG
-BATCH_SIZE = 120
-ACTOR_LR = 1e-5
+BATCH_SIZE = 128
+ACTOR_LR =1e-4
 CRITIC_LR = 1e-3
 GAMMA = 0.99
 
-# Hyper paramters Noise
+# Hyper parameters for the noise
 REGULATION_COEFFICENT = 1 # only moves the variance (if =1: sigma stays the same)
 DECAY_RATE = 1 # basically no decay rate gets apllied
-NOISE_VARIANCE=4
+NOISE_VARIANCE = 4
 
 # Random Guassian Noise gets added to the actions for exploratation 
 noise = GaussianNoise(env.action_space, mu=0, sigma=NOISE_VARIANCE, regulation_coef=REGULATION_COEFFICENT, decay_rate=DECAY_RATE)
-
 
 # Reset the environment
 env.reset()
@@ -46,7 +46,6 @@ done = False
 
 # Create agent-ddpg
 "increase max memory size ?"
-
 agent = agent_ddpg(env, hidden_size=[400, 300], actor_learning_rate=ACTOR_LR, critic_learning_rate=CRITIC_LR, gamma=GAMMA, tau=1e-3, max_memory_size=50000, norm = 'none')
 
 # comet logging
@@ -58,8 +57,16 @@ experiment.log_parameter('Learning Rate (Actor)', ACTOR_LR)
 experiment.log_parameter('Learning Rate (Critic)',CRITIC_LR)
 experiment.log_parameter('Gamma', GAMMA)
 
+# Save outcomes as lists
+all_rewards = []
+all_obs = []
+all_next_obs = []
+all_actions = []
+all_done = []
+
 # Training
 for step in range(1000):
+    cum_rewards = 0
     env.reset()
     done = False 
     
@@ -68,21 +75,26 @@ for step in range(1000):
     while not done:
         
         action = agent.get_action(np.asarray([env.state.to_observation()[0]]))
-        #action = agent.get_action(env.state.to_observation())
         action = noise.get_action(action)
-        print(action)
+        #print(action)
         
         # Specify the action. Check the effect of any fixed policy by specifying the action here:
         observation, reward, done, _ = env.step(action)
         
-        #Saves the played round as tuple in the memory
-        agent.memory.push(tuple(map(operator.sub, observation, (1,))), action, reward, observation, done)
+        all_rewards.append(reward)
+        all_obs.append(tuple(map(operator.sub, observation, (1,))))
+        all_next_obs.append(observation)
+        all_actions.append(action)
+        all_done.append(done)
         
-        if len(agent.memory) > BATCH_SIZE:
-            agent.update(BATCH_SIZE)
-    
-    #if len(agent.memory) > BATCH_SIZE:
-        #agent.update(BATCH_SIZE)
+        cum_rewards += reward
+        
+    #Saves the played round as tuple in the memory
+    for i in range(len(all_rewards)):
+        agent.memory.push(all_obs[i], all_actions[i], all_rewards[i]/cum_rewards, all_next_obs[i], all_done[i])
+        
+    if len(agent.memory) > BATCH_SIZE:
+        agent.update(BATCH_SIZE)
         
         
 # Testing/Evaluation
@@ -94,13 +106,10 @@ while not done:
     
     action = agent.get_action(np.asarray([env.state.to_observation()[0]]))
     
+    print(action)
     # Specify the action. Check the effect of any fixed policy by specifying the action here:
     observation, reward, done, _ = env.step(action)
     
-    agent.memory.push(tuple(map(operator.sub, observation, (1,))), action, reward, observation, done)
-    
-   #if len(agent.memory) > BATCH_SIZE: # necessary??
-        #agent.update(BATCH_SIZE)
 
 
 # Plot the episode
